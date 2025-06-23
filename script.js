b9ab26a95f0c84383a32d771803313217c4f1237
Jasmin Silva <jasminsilva498@gmail.com>
2025-06-23 15:49:22 -0300

    Substituindo as imagens de perfil, de acordo com o modo

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 function toggleMode() {
   const html = document.documentElement
-
-  /*if (html.classList.contains("Light")) {
-    html.classList.remove("Light")
-  } else {
-    html.classList.add("Light")
-  }*/
-
   html.classList.toggle("Light")
+
+  //pegar a tag img
+  const img = document.querySelector("#profile img")
+  //substituir a imagem
+
+  if (html.classList.contains("Light")) {
+    //se tiver light mode, adicionar a imagem light
+    img.setAttribute("src", "./assets/avatar-light.png")
+  } else {
+    //se tiver sem light mode, manter a imagem normal
+    img.setAttribute("src", "./assets/avatar.png")
+  }
+
   //Faz a mesma que está na condicional acima comentada
 }
